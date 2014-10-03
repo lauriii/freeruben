a277c1e3cf0106beb3269a3386e5ac7ce50ae986
Lauri Eskola <lauri.eskola@gmail.com>
2014-10-04 02:16:10 +0300

    Stuff

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 "use strict";
-var easter_egg = new Konami( function() { ruben_attack(); } );
+var code = true;
+for (var i = 0; i < code.length; i++) {
+  var sequence = (code == true) ? [38, 38, 40, 40, 37, 39, 37, 39, 66, 65] : code;
+  var easter_egg = new Konami( function() { ruben_attack(); }, sequence);
+}
 
 function ruben_attack() {
   // Subtract Druplicon width and height to ensure that he is only spawned
