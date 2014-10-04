fbba27325b9a348e0b55e420225e30f05420391f
Lauri Eskola <lauri.eskola@gmail.com>
2014-10-04 03:06:32 +0300

    Stuff

--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
 "use strict";
-var easter_egg = new Konami( function() { ruben_attack(); } );
+var deepExtend = function(out) {
+  out = out || {};
+
+  for (var i = 1; i < arguments.length; i++) {
+    var obj = arguments[i];
+
+    if (!obj)
+      continue;
+
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        if (typeof obj[key] === 'object')
+          deepExtend(out[key], obj[key]);
+        else
+          out[key] = obj[key];
+      }
+    }
+  }
+
+  return out;
+};
+
+var Konami = new Konami();
+
+var code = [[75, 73, 84, 84, 69, 78], [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]];
+for (var i = 0; i < code.length; i++) {
+  var sequence = (code[i] == true) ? [38, 38, 40, 40, 37, 39, 37, 39, 66, 65] : code[i];
+  console.log(sequence);
+  var Konami = new Konami();
+  Konami.konami.addEvent(code[i]);
+  //var easter_egg = new Konami( function() { ruben_attack(); }, sequence);
+}
 
 function ruben_attack() {
   // Subtract Druplicon width and height to ensure that he is only spawned
